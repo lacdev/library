753be687914527866ada2485d005f5de09e96577
German <german.mtz@protonmail.com>
2021-11-14 23:29:15 -0600

    initial constructor function tests

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Library Project")
+console.log('Library Project')
+
+function Book(name, author, pages, read) {
+  this.name = name
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function () {
+    if (read === true) {
+      read = 'Read book'
+    } else {
+      read = 'Not read yet'
+    }
+    return `${name} by ${author}, ${pages} pages, ${read}`
+  }
+}
+
+const book1 = new Book('Algorithms to Live By', 'Brian Christian', 368, true)
+
+console.log(book1)
+
+const book2 = new Book('Clean Code', 'Robert C. Martin', 464, false)
+
+console.log(book2)
