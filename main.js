4d0a56851542b820381684b176463ce0823e6c21
German <german.mtz@protonmail.com>
2021-11-15 18:40:55 -0600

    add test form with basic validation for user inputs

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log('Library Project')
 
 const myLibrary = []
+
 const Book = function (name, author, pages, read) {
   this.name = name
   this.author = author
@@ -8,12 +9,31 @@ const Book = function (name, author, pages, read) {
   this.read = read
 }
 
-Book.prototype.info = function () {
+// Book.prototype.info = function () {
+//   if (this.read) {
+//     return `${this.name} by ${this.author}, ${this.pages} pages, I read the book already`
+//   } else {
+//     return `${this.name} by ${this.author}, ${this.pages} pages, I haven't read the book.`
+//   }
+// }
+
+Book.prototype.toggleReadStatus = function () {
   if (this.read) {
-    return `${this.name} by ${this.author}, ${this.pages} pages, I read the book already`
+    return
   } else {
-    return `${this.name} by ${this.author}, ${this.pages} pages, I haven't read the book.`
   }
 }
 
-const book1 = Object.create(Book)
+const addBookToLibrary = function () {}
+
+const buildBook = function (book) {
+  const { name, author, pages, read } = book
+
+  const book1 = Object.create(Book)
+}
+
+const renderBooks = function (array) {
+  array.forEach((book) => {
+    buildBook(book)
+  })
+}
